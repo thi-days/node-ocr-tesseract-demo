b6be3cd82aa9d5971c77abd69f10b8d25866633f
Thiago Dias <thiagodbrt@gmail.com>
2021-11-01 14:05:11 -0300

    Add environment file

--- src/api/welcome.api.ts
+++ src/api/welcome.api.ts
@@ -4,7 +4,6 @@ export const welcomeApi: Router = Router();
 
 welcomeApi.all('', (req: Request, res: Response) => {
   res.send(
-    `Welcome to Node OCR Tesseract Demo. =)\n
-     Send a POST request to /read-image. For more information, read the readme.md file at this project. =)`
+    'Welcome to Node OCR Tesseract Demo. =)\nSend a POST request to /tesseract/read-image. For more information, read the readme.md file at this project. =)'
   );
 });
