67ddf9d12d3bf20b56994654e110cd8fd27bf8fc
Thiago Dias <thiagodbrt@gmail.com>
2022-08-31 19:09:09 -0300

    Updates postman collection and package json

--- src/api/welcome.api.ts
+++ src/api/welcome.api.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 export const welcomeApi: Router = Router();
 
-welcomeApi.all('', (req: Request, res: Response) => {
+welcomeApi.all('', (_: Request, res: Response) => {
   res.send(
     'Welcome to Node OCR Tesseract Demo. =)\nSend a POST request to /tesseract/read-image. For more information, read the readme.md file at this project. =)'
   );
